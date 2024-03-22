eb1b2cdd3970bef0589fab436a06524d060ceb10
Dhanonjoy Saha Anogh <dhanonjoy6a24@gmail.com>
2024-03-22 20:38:44 +0600

    first project

---
+++ main.py
@@ -0,0 +1,12 @@
+def game():
+  name = input("Your name: ")
+  verb1 = input("Verb: ")
+  verb2 = input("Verb: ")
+  adj1 = input("Adjective: ")
+  adj2 = input("Adjective: ")
+  madlib = f"Hello my name is {name}. I like {verb1} and {verb2}. {verb1} is so {adj1} \
+ and I also {adj2} programming."
+  print(madlib)
+
+
+game()
