705ba24e3e2428b50a0b5e72d4abe5544339ed5c
Dhanonjoy Saha Anogh <dhanonjoy6a24@gmail.com>
2024-03-24 20:38:07 +0600

    3rd work

--- guess the num(Computer).py
+++ guess the num(Computer).py
@@ -13,13 +13,34 @@ def guess(x):
   print("Yay, You guessed the number correctly!!")
 
 
+def computer_guess(x):
+  low = 1
+  high = x
+  feedback = ""
+  while feedback != "c":
+    if low != high:
+      guess = random.randint(low, high)
+    else:
+      guess = low # 'cuz low = high
+    feedback = input(f"Is {guess} too high (h), too low (l) or correct (c): ").lower()
+    if feedback == "h":
+      high = guess - 1
+    elif feedback == "l":
+      low = guess + 1
+  
+  print(f"Yay, computer guessed your number, {guess}, correctly!!")
+
+
 def game_chooes():
-  game_name = input("Enter the game name: ")
+  game_name = input("Enter the game name (Madlib, Guess(User), Guess(Computer)): ")
   if game_name == "Madlib":
     madlib()
-  elif game_name == "Guess(Computer)":
+  elif game_name == "Guess(User)":
     x = int(input("Range: "))
     guess(x)
+  elif game_name == "Guess(Computer)":
+    x = int(input("Range: "))
+    computer_guess(x)
   else:
     print("Sorry, that's all we have.")
 
