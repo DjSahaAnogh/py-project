116bcddef2a6ab393464a00b5bcd670130624b2d
Dhanonjoy Saha Anogh <dhanonjoy6a24@gmail.com>
2024-08-16 12:26:11 +0600

    .

---
+++ revers_str.py
@@ -0,0 +1,19 @@
+x = input("Srt: ")
+new = x[::-1]
+print(x)
+print(new)
+
+
+
+# def check_prime(n): 
+#   for i in range(2, n):
+#     if n % i == 0:
+#       return False
+#   else:
+#     return True
+
+
+# for p in range(1,100):
+#   if check_prime(p):
+#     ans_prime = 17*p+7
+#     print(ans_prime)
